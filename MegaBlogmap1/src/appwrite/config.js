62f406e5b63542217f23586390f80e18a6ef19d8
devanshV18 <devanshverma024@gmail.com>
2023-11-18 18:55:41 +0530

    database functionalities set

--- MegaBlogmap1/src/appwrite/config.js
+++ MegaBlogmap1/src/appwrite/config.js
@@ -13,6 +13,76 @@ export class DbService{
         this.databases = new Databases(this.client); 
         this.bucket = new Storage(this.client);
     }
+
+    async createPost({title,slug,content,featuredImage,status,userId}){
+        try {
+            return await this.databases.createDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+        } catch (error) {
+            console.log("appwrite error", error);
+        }
+    }
+
+    async updatePost(slug,{title,content,featuredImage,status}){
+        try {
+            return await this.databases.updateDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service BT", error);
+        }
+
+    }
+
+    async deletePost (slug) {
+        try {
+            await this.databases.deleteDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug
+            )
+
+            return true
+
+        } catch (error) {
+            console.log("appwrite error",error);
+
+            return false
+        }
+
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            console.log("Appwrite error",error);
+            return false;
+        }
+
+    }
 }
 
 const service = new DbService()
