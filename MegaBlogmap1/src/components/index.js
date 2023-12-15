9467bfa805c04b36c7eb8bddd7660ecad1f1512f
devanshV18 <devanshverma024@gmail.com>
2023-12-16 00:47:43 +0530

    Project complete debugging ongoing

--- MegaBlogmap1/src/components/index.js
+++ MegaBlogmap1/src/components/index.js
@@ -3,6 +3,15 @@ import Footer from "./Footer/Footer";
 import LogoutBtn from "./Header/LogoutBtn";
 import Container from "./Container/container";
 import Logo from "./Logo";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./Post-form/PostForm"
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
+import Button from "./Button"
+import Input from "./Input"
+import Select from "./Select"
 export {
-    Header,Footer,Container,Logo,LogoutBtn
+    Header,Footer,Container,Login,Logo,LogoutBtn,RTE,Signup,PostForm,PostCard,AuthLayout,Button,Input,Select
 }
